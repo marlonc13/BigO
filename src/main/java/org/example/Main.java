2c4b5a0848056c31656656ab2de2600defaa6a93
marlonc13 <marlonsoaresleite@gmail.com>
2026-09-18 17:49:06 -0300

    Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static Integer encontrarMaior(int[] numeros) {
+        if (numeros == null || numeros.length == 0) {
+            return null;
+        }
+        int maior = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+        }
+
+        return maior;
+    }
+    public static void main(String[] args) {
+        int[] numeros = {12, 45, 7, 89, 23, 56};
+        Integer resultado = encontrarMaior(numeros);
+        System.out.println("O maior número é: " + resultado);
+        int[] numeros1 = {34, 85, 12, 9, 24, 24, 63, 123, 53, 63, 123, 7654, 13, 75, 123, 75};
+        Integer resultado1 = encontrarMaior(numeros1);
+        System.out.println("O maior número é: " + resultado1);
+    }
+}
